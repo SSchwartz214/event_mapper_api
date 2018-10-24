d7ccc409267030d7a91fd99b2d3b6a4ec7263f6f
Seth Schwartz <sschwartz214@gmail.com>
2018-10-24 13:47:51 -0600

    adds cors and refactors controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create 
-    User.update_or_create(request.body)
+    user = User.update_or_create(user_params)
+    if user
+      render json: user
+    else
+      render status: 400
+    end
   end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:given_name, :family_name, :email, :google_id)
+    end
 end
