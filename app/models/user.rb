f5f164fb49434b39f71fb7a40570dc7d81fb49cb
Seth Schwartz <sschwartz214@gmail.com>
2018-10-23 21:05:18 -0600

    adds route for user create and method to add user to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
-    validates_presence_of :first_name, :last_name, :email, :gid
+  validates_presence_of :first_name, :last_name, :email, :gid
+
+  def self.update_or_create(auth)
+    user = User.find_by(gid: auth[:google_id] || User.new)
+    user.attributes = {
+      first_name: auth[:given_name],
+      last_name: auth[:family_name],
+      email: auth[:email],
+      gid: auth[:google_id]
+    }
+      user.save!
+      user      
+  end
 end
