2331d55b415260f9988a54a78372aaa6c2fdb8f7
Seth Schwartz <sschwartz214@gmail.com>
2018-10-23 21:42:05 -0600

    Creates events and user events table, sets up relationships,

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_021713) do
+ActiveRecord::Schema.define(version: 2018_10_24_030641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_events", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -24,4 +38,6 @@ ActiveRecord::Schema.define(version: 2018_10_24_021713) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it {should validate_presence_of(:first_name)}
-  it {should validate_presence_of(:last_name)}
-  it {should validate_presence_of(:email)}
-  it {should validate_presence_of(:gid)}
+  describe "Validations" do
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:gid)}
+  end
+
+  describe "Relationships" do
+    it {should have_many :user_events}
+    it {should have_many(:events).through(:user_events)}
+  end
+
+  describe "Class Methods" do
+    it "creates or updates itself from an oauth hash" do
+      auth = {
+        given_name: "Seth",
+        family_name: "Schwartz",
+        email: "seth@gmail.com",
+        google_id: "12345"
+      }
+
+      User.update_or_create(auth)
+      new_user = User.first
+  
+      expect(new_user.first_name).to eq("Seth")
+      expect(new_user.last_name).to eq("Schwartz")
+      expect(new_user.email).to eq("seth@gmail.com")
+      expect(new_user.gid).to eq("12345")
+    end
+  end
 end
