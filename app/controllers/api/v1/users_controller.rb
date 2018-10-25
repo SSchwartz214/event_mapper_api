aba37397e8d0718285bcfc62a6d7e4bb8949d118
Seth Schwartz <sschwartz214@gmail.com>
2018-10-24 21:12:16 -0600

    updates code for CORS

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  protect_from_forgery with: :null_session
-  
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+
   def index
     render json: User.all
   end
