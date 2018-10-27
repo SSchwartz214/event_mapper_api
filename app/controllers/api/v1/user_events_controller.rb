9091d4f2dac322082ca3dcde0a2eeed2c1f6afea
Seth Schwartz <sschwartz214@gmail.com>
2018-10-27 13:56:59 -0600

    get user_events WIP

--- app/controllers/api/v1/user_events_controller.rb
+++ app/controllers/api/v1/user_events_controller.rb
@@ -1,20 +1,25 @@
 class Api::V1::UserEventsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
 
-def destroy
-  user = User.find(params[:user_id])
-  event = Event.find(params[:id])
-
-  if user && event && UserEvent.find_by(user: user, event: event)
-    user_event = UserEvent.find_by(user: user, event: event)
-    user_event.destroy
-    render json: {
-      user: user.id,
-      event: event.e_id
-    }
-  else
-    render json: {error: 'Unable to delete watch list entry'}, status: 404
+  def index
+    events = Event.where
+    render json: user_events
   end
-end
+
+  def destroy
+    user = User.find(params[:user_id])
+    event = Event.find(params[:id])
+
+    if user && event && UserEvent.find_by(user: user, event: event)
+      user_event = UserEvent.find_by(user: user, event: event)
+      user_event.destroy
+      render json: {
+        user: user.id,
+        event: event.e_id
+      }
+    else
+      render json: {error: 'Unable to delete watch list entry'}, status: 404
+    end
+  end
 
 end
--- spec/requests/user_event_request_spec.rb
+++ spec/requests/user_event_request_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 describe "User Events API" do
+
+  it 'can send a list of all the user_events' do
+    seth = create(:user)
+    concert_1 = seth.events.create(name: 'Drake', e_id: '123', url: 'www.drake.com', img: 'drake.jpeg', date: "Sun, Dec 9, 2018 6:00 PM", venue_name: "Pepsi Center", address: "2009 Larimer St Denver, CO 80205", lat: "39.1", lng: "-11.2", distance: "1.3")
+    concert_2 = seth.events.create(name: 'Sia', e_id: '1234', url: 'www.drake.com', img: 'drake.jpeg', date: "Sun, Dec 9, 2018 6:00 PM", venue_name: "Pepsi Center", address: "2009 Larimer St Denver, CO 80205", lat: "39.1", lng: "-11.2", distance: "1.3")
+
+    get "/api/v1/users/#{seth.id}/events"
+
+    expect(response).to be_successful
+
+    watch_list = JSON.parse(response.body)
+
+    expect(watch_list.count).to eq(2)
+  end
+
   it 'can destroy a record from the user_events table' do
     seth = create(:user)
     concert = seth.events.create(name: 'Drake', e_id: '123', url: 'www.drake.com', img: 'drake.jpeg', date: "Sun, Dec 9, 2018 6:00 PM", venue_name: "Pepsi Center", address: "2009 Larimer St Denver, CO 80205", lat: "39.1", lng: "-11.2", distance: "1.3")
