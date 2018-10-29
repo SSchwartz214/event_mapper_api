35d412e44657faabb4bf930bdefdbc4cfe90c136
Seth Schwartz <sschwartz214@gmail.com>
2018-10-29 11:09:21 -0600

    adds imageUrl to users table

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -25,6 +25,6 @@ class Api::V1::EventsController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:given_name, :family_name, :email, :google_id) 
+      params.require(:user).permit(:given_name, :family_name, :email, :google_id, :imageUrl) 
     end
 end
