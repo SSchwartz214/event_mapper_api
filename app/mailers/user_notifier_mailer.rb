195a3715009d91dab556659b7f6983c913e2263e
Seth Schwartz <sschwartz214@gmail.com>
2018-10-31 16:15:00 -0600

    adds arguments to send_watchlist_email method

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,7 +1,7 @@
 class UserNotifierMailer < ApplicationMailer
-  def send_watchlist_email
+  def send_watchlist_email(user, email)
     @user = user
     @event = event 
-    mail(to: user.email, subject:  "#{event} added to your EventMapper watch list" )
+    mail(to: user.email, subject:  "#{event.name} added to your EventMapper watch list" )
   end
 end
